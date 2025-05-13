5f1b34ab636e5c5e6bb44bfe24af02946acae864
ViniciusMoresqui <viniciusgiratomoresqui@gmail.com>
2025-05-13 20:41:52 -0300

    Atualização do cadastro
    Fizemos atualização do cadastro na parte de javascript, onde fizemos atualização do usuário atráves do storage ou lista, onde vai ficar aguarda no vetor para não apagar, adicionamos botão remover, outra coisa vamos arrumar sobreescrita que ocorre após recarregar a pagina.

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,15 +1,38 @@
 // cadastro
 const botao = document.getElementById("btnCadastrar");
+let usuarios = [];
 
+// cadastrar
 botao.addEventListener('click',
     function (){
         const usuario = {
             login: document.getElementById('login').value,
             senha: document.getElementById('senha').value
         }
-        let usuarios = [];
         usuarios.push(usuario);
             let listaUsuarios = JSON.stringify(usuarios);
             localStorage.setItem("usuarios", listaUsuarios);
+            listar();
     }
 );
+
+function listar(){
+    const listaUsuarioCad = JSON.parse(localStorage.getItem("usuarios")) || [];
+    console.log(listaUsuarioCad);
+    const tabelaListaUsuarios = document.getElementById('listaUsuario');
+    tabelaListaUsuarios.innerHTML = "";
+
+    listaUsuarioCad.forEach((usuario, index) => {
+        const linha = document.createElement('tr');
+        linha.innerHTML = `
+            <td>${usuario.login}</td>
+            <td>${usuario.senha}</td>
+            <td>
+                <button onclick"excluirUsuario(${index})">Remover</button>
+            </td>
+        `;
+        tabelaListaUsuarios.appendChild(linha);
+    });
+}
+
+listar();
