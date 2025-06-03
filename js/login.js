2fededb73a19cbd053d87be4318e65d1ac1c5b07
ViniciusMoresqui <viniciusgiratomoresqui@gmail.com>
2025-06-03 19:33:50 -0300

    Finalizado.

--- js/login.js
+++ js/login.js
@@ -4,12 +4,16 @@ const botao = document.getElementById('entrar');
 botao.addEventListener('click',
     function (){
         const usuario = JSON.parse(localStorage.getItem("usuarios")) || [];
+        let login = document.getElementById("login").value;
+        let senha = document.getElementById("senha").value;
 
-        const login = usuario.find(user => user.login === login && user.senha === senha);
-
-        
-
+        const logado = usuario.find(user => user.login === login && user.senha === senha);
+    
+        if(logado){
+            localStorage.setItem('userLogado', login);
+            window.location.href = "dashbord.html";
+        }else{
+            document.getElementById('mensagemErro').textContent = "Login e Senha incorretos";
+        }
     }
-
-
 );
